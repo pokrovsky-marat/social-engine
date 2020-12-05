5b0b4719a9f5bef22fbd3bffa2fea55f611fc950
Marat Isaev <fake@mail.com>
2020-12-05 18:05:40 +0600

    lesson 56

---
+++ src/components/Users/PresentUsers.jsx
@@ -0,0 +1,50 @@
+import s from "./Users.module.css";
+import React from "react";
+import img from "./../../assets/pics/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png";
+
+let PresentUsers = (props) => {
+  let countPagesArray = [];
+  let countPages = Math.ceil(props.totalCount / props.pages);
+  for (let i = 1; i <= countPages; i++) {
+    countPagesArray.push(i);
+  }
+
+  return (
+    <div className={s.users}>
+      <div>
+        {countPagesArray.map((item, index) => (
+          <button
+            className={item === props.numberSheet ? s.btn : ""}
+            onClick={() => {
+              props.goToPage(item);
+            }}
+            key={index}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+      {props.users.map((item) => (
+        <div key={item.id} className={s.user}>
+          <div>id: {item.id}</div>
+          <div>
+            <img src={item.photos.small || img} alt="ava" />
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                props.followBtnClick(item.id);
+              }}
+            >
+              {item.followed ? "Don't follow" : "Follow"}
+            </button>
+          </div>
+          <div>{item.name}</div>
+          <div>{status}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PresentUsers;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,35 +1,92 @@
-import React from 'react'
-import {addDialogActionCreator, changeDialogActionCreator} from "../../redux/dialogsPageReducer";
-import Users from "./Users";
-import {connect} from "react-redux";
+import React from "react";
+import * as axios from "axios";
+import { connect } from "react-redux";
 import {
-    changeFollowUserActionCreator,
-    changeNumberSheetActionCreator,
-    setStateActionCreator
+  changeFollowUserActionCreator,
+  changeNumberSheetActionCreator,
+  setStateActionCreator,
 } from "../../redux/usersPageReducer";
+import PresentUsers from "./PresentUsers";
+
+class UsersContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.followBtnClick = this.followBtnClick.bind(this);
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.numberSheet}&count=${this.props.pages}`
+      )
+      .then((response) => {
+        console.log("request to server from componentDidMount");
+
+        this.props.setState(response.data);
+      });
+  }
+
+  followBtnClick(value) {
+    this.props.changeFollow(value);
+  }
+  // followBtnClick(value) {
+  //   debugger;
+  //   this.props.changeFollow(value);
+  //   //TypeError: Cannot read property 'changeFollow' of undefined
+  // }
+  // followBtnClick = (value) => {
+  //   debugger;
+  //   this.props.changeFollow(value);
+  // };
+
+  goToPage = (pageNumber) => {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pages}`
+      )
+      .then((response) => {
+        console.log("request to server from goToPage");
+        this.props.changeNumberSheet(pageNumber, response.data);
+      });
+  };
+
+  render() {
+    return (
+      <PresentUsers
+        // props={this.props}
+        totalCount={this.props.totalCount}
+        pages={this.props.pages}
+        numberSheet={this.props.numberSheet}
+        goToPage={this.goToPage}
+        users={this.props.users}
+        followBtnClick={this.followBtnClick}
+      />
+    );
+  }
+}
 
 let mapStateToProps = (state) => {
-    return {
-        users: state.usersPage.users,
-        totalCount: state.usersPage.totalCount,
-        pages: state.usersPage.pages,
-        numberSheet: state.usersPage.numberSheet
-    }
-}
+  return {
+    users: state.usersPage.users,
+    totalCount: state.usersPage.totalCount,
+    pages: state.usersPage.pages,
+    numberSheet: state.usersPage.numberSheet,
+  };
+};
 let mapDispatchToProps = (dispatch) => {
-    return {
-        changeFollow: (id) => {
-            dispatch(changeFollowUserActionCreator(id))
-        },
-        setState: (state) => {
-            dispatch(setStateActionCreator(state))
-        },
-        changeNumberSheet: (number,state) => {
-            dispatch(changeNumberSheetActionCreator(number,state))
-        }
-    }
-}
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+  return {
+    changeFollow: (id) => {
+      dispatch(changeFollowUserActionCreator(id));
+    },
+    setState: (state) => {
+      dispatch(setStateActionCreator(state));
+    },
+    changeNumberSheet: (number, state) => {
+      dispatch(changeNumberSheetActionCreator(number, state));
+    },
+  };
+};
 
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
