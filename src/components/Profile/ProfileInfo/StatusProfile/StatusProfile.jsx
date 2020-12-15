6d3222e425be9bcfc921f3b958a17b644e30dd66
Marat Isaev <fake@mail.com>
2020-12-15 22:05:52 +0600

    lesson 74 componentDidUpdate

--- src/components/Profile/ProfileInfo/StatusProfile/StatusProfile.jsx
+++ src/components/Profile/ProfileInfo/StatusProfile/StatusProfile.jsx
@@ -11,30 +11,36 @@ class StatusProfile extends React.Component {
         };
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        debugger
+        if (this.props.statusProfile !== prevProps.statusProfiley) {
+            this.setState({statusText: this.props.statusProfile})
+        }
+
+    }
 
     editStatus = () => {
         this.setState({editMode: true})
     }
+    changeText = (e) => {
+        debugger
+        this.setState({statusText: e.target.value})
+    }
     finishEdit = (e) => {
-        this.setState({editMode: false, statusText: e.target.value})
-        this.props.updateStatusProfile(e.target.value);
+        this.setState({editMode: false})
+        this.props.updateStatusProfile(this.state.statusText);
     }
     changeState = () => {
         this.setState({statusText: this.props.statusProfile})
     }
 
     render() {
-        if (this.state.statusText !== this.props.statusProfile) {
-            console.log('***************');
-            this.changeState()
-
-        }
         return <div className={styles.status}>
 
             {!this.state.editMode && <span
                 onDoubleClick={this.editStatus}>{this.props.statusProfile || 'Double click and input your status'}</span>}
             {this.state.editMode &&
-            <input autoFocus={true} onBlur={this.finishEdit} defaultValue={this.state.statusText} type="text"/>}
+            <input onChange={this.changeText} autoFocus={true} onBlur={this.finishEdit} value={this.state.statusText} type="text"/>}
         </div>
     }
 }
